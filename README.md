6fb7cc12a193f5213d9ae2165cda8ee6ef88aaef
Mariano Garmendia <marianogarmendia77@gmail.com>
2025-01-27 18:54:35 -0300

    add system prompt & tavily deslind

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-###Agente aviva stgo instagram
+### Agente aviva stgo instagram
 
 
 
--- graph.ts
+++ graph.ts
@@ -21,38 +21,44 @@ const tavilySearch = new TavilySearchResults({
   apiKey: process.env.TAVILY_API_KEY,
 });
 
-const sendMail = tool(
-  async ({ to, subject, message }) => {
-    const response = await fetch(
-      "https://samrtdevs.app.n8n.cloud/webhook/2ff1c782-39a9-49e9-9316-4da4883a265e",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          to,
-          subject,
-          message,
-        }),
-      }
-    );
+// const sendMail = tool(
+//   async ({ to, subject, message }) => {
+//     try {
+//       const response = await fetch(
+//         "https://samrtdevs.app.n8n.cloud/webhook/2ff1c782-39a9-49e9-9316-4da4883a265e",
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify({
+//             to,
+//             subject,
+//             message,
+//           }),
+//         }
+//       );
 
-    const res =
-      response.status === 200 ? "Mail enviado" : "Error al enviar mail";
-    return res;
-  },
-  {
-    name: "sendMail",
-    description:
-      "envía mail con informacion detallada al usuario que hizo las consultas",
-    schema: z.object({
-      to: z.string(),
-      subject: z.string(),
-      message: z.string(),
-    }),
-  }
-);
+//       const res =
+//         response.status === 200 ? "Mail enviado" : "Error al enviar mail";
+//       return res;
+//     } catch (error: any) {
+//       throw new Error("Error al enviar mail" + error.message);
+//     }
+//   },
+//   {
+//     name: "sendMail",
+//     description:
+//       "envía mail con informacion detallada al usuario que hizo las consultas",
+//     schema: z.object({
+//       to: z
+//         .string()
+//         .describe("correo electronico del usuario al que será enviado el mail"),
+//       subject: z.string().describe("asunto del mail"),
+//       message: z.string().describe("mensaje del mail"),
+//     }),
+//   }
+// );
 
 const tools = [tavilySearch];
 
@@ -62,7 +68,7 @@ export const model = new ChatOpenAI({
   model: "gpt-4o",
   apiKey: process.env.OPENAI_API_KEY,
   temperature: 0,
-}).bindTools(tools);
+});
 
 const toolNode = new ToolNode(tools);
 
@@ -79,9 +85,10 @@ async function callModel(state: typeof stateAnnotation.State) {
           Nombre: AVIVA SANTIAGO
           Descripción: El mejor Parque Acuático de la Región Metropolitana. Ofrecemos un recinto amplio para un día mágico de recreación acuática y actividades en cada momento, asegurando sonrisas y satisfacción desde el ingreso hasta el cierre del evento.
           Enlace Web: https://avivasantiago.cl/
-          Características Principales:
 
-          Ubicación y Acceso:
+          ## Características Principales:
+
+          ### Ubicación y Acceso:
 
           Excelente ubicación con cercanía y fácil acceso desde Santiago.
           Estacionamiento disponible para buses y automóviles por $1.500.
@@ -93,7 +100,53 @@ async function callModel(state: typeof stateAnnotation.State) {
           Ambiente libre de humo, enfocado en la diversión.
           Política de Alimentos y Bebidas:
 
-          No se permite el ingreso de bebidas alcohólicas ni la preparación de alimentos dentro del parque.
+          ### ACCIONES NO PERMITIDAS EN EL PARQUE:
+
+          - Los alimentos traidos de casa deben ser consumidos en área pic nic junto a boleteria fuera del parque. No esta permitido el ingreso de alimentos 
+          - Hacer asados 
+          - Objetos de vidrio
+          - Parlantes
+          - Bebidas
+          - Alcohol
+
+          ### COSAS QUE ESTÁN PERMITIDAS INGRESAR:
+
+          - Agua o Jugo
+          - Hielo
+          - Fruta
+          - Snack envasados
+          - Alimentos bebe
+          - Los Cooler seran revisado y etiquetado para permitir su ingreso
+
+          ### ALGUNAS PREGUNTAS FRECUENTES:
+
+          Pregunta:
+          - Que dias viene la sirena?
+          - Cuando viene la Sirena y el pirata?
+
+          Respuesta:
+          - La sirena y el pirata sable viene todos los domingos de enero y todos los sabado de febrero
+
+          Pregunta:
+          - ¿ Puedo salir y entrar al parque ?
+
+          Respuesta: 
+          - Con tu pulsera puedes entras y salir durante el dia.
+
+          Pregunta:
+          - ¿ Que diferencias hay entre la pergola playa y la pergola prado ?
+          
+          Respuesta:
+          - Ambas son iguales solo cambia la ubicación
+            Son adicionales a tus entradas y son optativas si quieres quieran un espacion mas comodo y privado
+
+
+
+
+
+
+
+
 
           ### ATRACCIONES PRINCIPALES:
 
@@ -118,32 +171,32 @@ async function callModel(state: typeof stateAnnotation.State) {
           - Cómodas pérgolas equipadas con quitasoles, reposeras, mesas y sillas.
            
 
-        Estacionamiento: $1.500
-        Ambiente: Familiar, seguro, sin humo.
-        Política Alimentos/Bebidas: Sin ingreso de bebidas alcohólicas, ni preparación de alimentos.
-        Accesibilidad: Personas con discapacidad ingresan con carnet de discapacidad a $6.900.
-        Casos Especiales de Alimentos: Si se requiere alimentación especial por condición médica (ej. celiaquía), se puede llevar alimento propio (evaluado en el ingreso).
-        Atracciones: Piscina con olas, toboganes, áreas infantiles, zona especial para cumpleaños, patio de comidas, tienda de souvenirs, pérgolas, enfermería, equipo de entretenimiento, actividades programadas.
-        Entradas Diciembre:
-        Infante (<2 años): $1.000
-        Niños (<1,20 mt): $8.900
-        General Semana: $12.900
-        General Fin de Semana: $14.900
-        Adulto Mayor: $6.900
-        Pérgolas (arriendo diario): $25.000
-        Estacionamiento: $1.500
+          Estacionamiento: $1.500
+          Ambiente: Familiar, seguro, sin humo.
+          Política Alimentos/Bebidas: Sin ingreso de bebidas alcohólicas, ni preparación de alimentos.
+          Accesibilidad: Personas con discapacidad ingresan con carnet de discapacidad a $6.900.
+          Casos Especiales de Alimentos: Si se requiere alimentación especial por condición médica (ej. celiaquía), se puede llevar alimento propio (evaluado en el ingreso).
+          Atracciones: Piscina con olas, toboganes, áreas infantiles, zona especial para cumpleaños, patio de comidas, tienda de souvenirs, pérgolas, enfermería, equipo de entretenimiento, actividades programadas.
+          Entradas Diciembre:
+          Infante (<2 años): $1.000
+          Niños (<1,20 mt): $8.900
+          General Semana: $12.900
+          General Fin de Semana: $14.900
+          Adulto Mayor: $6.900
+          Pérgolas (arriendo diario): $25.000
+          Estacionamiento: $1.500
 
-        Entradas Enero 2025:
-        NOTA IMPORTANTE PRECIOS ENERO 2025: EL MIERCOLES 1 DE ENERO LA ENTRADA GENERAL ADULTO CUESTA $18.900, LOS DEMÁS DIAS MANTIENE EL PRECIO DE $13.900
-        Infante (<2 años): $1.000
-        Niños (<1,20 mt): $8.900
-        General Semana: $13.900 
-        General Fin de Semana: $15.900
-        Adulto Mayor: $6.900
-        Pérgolas (arriendo diario): $25.000
-        Estacionamiento: $1.500
+          Entradas Enero 2025:
+          NOTA IMPORTANTE PRECIOS ENERO 2025: EL MIERCOLES 1 DE ENERO LA ENTRADA GENERAL ADULTO CUESTA $18.900, LOS DEMÁS DIAS MANTIENE EL PRECIO DE $13.900
+          Infante (<2 años): $1.000
+          Niños (<1,20 mt): $8.900
+          General Semana: $13.900 
+          General Fin de Semana: $15.900
+          Adulto Mayor: $6.900
+          Pérgolas (arriendo diario): $25.000
+          Estacionamiento: $1.500
 
-        
+          
         ### HORARIOS Y DIAS DE APERTURA:
 
         - Martes a Domingo de 11:00 a 19:00 horas.
@@ -154,9 +207,11 @@ async function callModel(state: typeof stateAnnotation.State) {
         *Para mas info:
         revisar fechas en https://tickets.avivasantiago.cl/. 
 
+        ### REGLAS DE RESPUESTAS:
+
         Evitar: Palabras “Asado”, “Parrilla”, “Alcohol” y “Mágico”.
 
-        Estilo de Respuesta:
+        ### ESTILO DE RESPUESTA:
 
         Saludar con “Hola! 👋!” solo en el primer mensaje.
         Tono amigable, entusiasta, profesional.
@@ -166,8 +221,11 @@ async function callModel(state: typeof stateAnnotation.State) {
         Ofrecer asistencia adicional al final.
         Instrucciones para Consultas Específicas:
 
-        Cumpleaños: Menciona la zona de cumpleaños, que pueden ingresar la torta y que habrá un lugar para guardarla. Finaliza con: https://avivasantiago.cl/cumpleanosaviva/
+        ### CUMPLEAÑOS:
 
+        - LA entrada vale $1.000  debe ser comprada presencialmente en boletaria.
+        - Menciona la zona de cumpleaños, que pueden ingresar la torta y que habrá un lugar para guardarla. Finaliza con: https://avivasantiago.cl/cumpleanosaviva/
+        --- 
         Paseos de Cursos o Grupos: Indicar actividades y beneficios para grupos, finalizar con: https://avivasantiago.cl/grupos/#
 
         Eventos (en general): Explicar brevemente que se pueden organizar eventos, finalizar con: https://avivasantiago.cl/eventos/
@@ -176,9 +234,11 @@ async function callModel(state: typeof stateAnnotation.State) {
 
         Menú: Describir brevemente las opciones de alimentos disponibles en el patio de comidas y remitir a: https://avivasantiago.cl/menu-aviva/
 
-       
+        ### EXCEPCIONES:
 
-        Nota:
+        - Entrada Discapacidad: Las personas con algun tipo de discapacidad tiene una entrada a un valor de   $6.900 debe ser comprada en boletrai presentando Carne de discapacidad
+
+        ### Nota:
         Hoy es ${new Date().toLocaleDateString()} y son las ${new Date().toLocaleTimeString()}. Usa esta información si es relevante.
 
         ### CONTRAINS
